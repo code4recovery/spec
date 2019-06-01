2e7097013d43d0ec7461e7399a9146d6f310bb61
Josh Reisner <josh@joshreisner.com>
2019-06-01 08:54:33 -0700

    finishing broward script

--- broward-county/meeting-guide.php
+++ broward-county/meeting-guide.php
@@ -8,7 +8,38 @@ function dd($obj) {
 }
 
 //need these for looping
-$days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+$days = array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
+$type_lookup = array(
+    'O' => 'O',
+    'C' => 'C',
+    'ag' => 'A',
+    'AB' => 'ABSI',
+    'BG' => 'BE',
+    'BB' => 'B',
+    'CB' => 'LIT',
+    //'CC' => 'Chair's Choice',
+    'DR' => 'DR',
+    'D' => 'D',
+    'FR' => 'FR',
+    'g' => 'G',
+    'GV' => 'GR',
+    'LT' => 'LIT',
+    'LS' => 'LS',
+    'M' => 'MED',
+    'm' => 'M',
+    'PG' => 'POR',
+    //'RF' => 'Rotating Format',
+    'RU' => 'RUS',
+    'SH' => 'S',
+    'SP' => 'SP',
+    'SPD' => array('SP', 'D'),
+    'ST' => 'ST',
+    'SS' => 'ST',
+    'STR' => array('ST', 'TR'),
+    'TR' => 'TR',
+    'w' => 'W',
+    'YP' => 'Y',
+);
 
 if (file_exists('wp-config.php')) {
 	include('wp-config.php');
@@ -36,7 +67,7 @@ $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
 //select location data
 try {
-	$result = $pdo->query('SELECT ID, post_title, post_modified_gmt FROM ' . $table_prefix . 'posts WHERE post_type = "locations" AND post_status = "publish"');
+	$result = $pdo->query('SELECT ID, post_title, post_name, post_modified_gmt FROM ' . $table_prefix . 'posts WHERE post_type = "locations" AND post_status = "publish"');
 	//$meetings = $pdo->query('SELECT ID, post_title, post_modified_gmt FROM ' . $table_prefix . 'posts WHERE post_type = "meetings" AND post_status = "publish"');
 	//$meta = $pdo->query('SELECT post_id, meta_key, meta_value FROM ' . $table_prefix . 'postmeta WHERE post_id IN (SELECT ID FROM ' . $table_prefix . 'posts WHERE meta_key NOT LIKE "\_%" AND post_type IN ("meetings", "locations"))');
 } catch (PDOException $e) {
@@ -44,12 +75,13 @@ try {
 }
 
 //define some arrays
-$locations = $meetings = array();
+$locations = $groups = $meetings = array();
 
 //build an array of locations, keyed by id
 foreach ($result as $r) {
 	$locations[$r->ID] = array(
 		'location' => $r->post_title,
+		'location_slug' => $r->post_name,
 		'location_updated' =>  $r->post_modified_gmt,
 	);
 }
@@ -67,79 +99,80 @@ $result = $pdo->query('SELECT ID, post_title, post_modified_gmt FROM ' . $table_
 
 //build an array of meetings, keyed by id
 foreach ($result as $r) {
-	$meetings[$r->ID] = array(
+	$groups[$r->ID] = array(
 		'name' => $r->post_title,
 		'meeting_updated' =>  $r->post_modified_gmt,
 	);
 }
 
-//get meeting metadata
-$result = $pdo->query('SELECT post_id, meta_key, meta_value FROM ' . $table_prefix . 'postmeta WHERE post_id IN (' . implode(',', array_keys($meetings)) . ')');
+//get group metadata
+$result = $pdo->query('SELECT post_id, meta_key, meta_value FROM ' . $table_prefix . 'postmeta WHERE meta_key NOT LIKE "\_%" AND post_id IN (' . implode(',', array_keys($groups)) . ')');
 
-$days = array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
-
-//attach meta values to meetings
+//attach meta values to group
 foreach ($result as $r) {
 	if ($r->meta_key == 'meeting_location') {
 		//attach location info to meeting
 		if (array_key_exists($r->meta_value, $locations)) {
-			$meetings[$r->post_id] = array_merge($meetings[$r->post_id], $locations[$r->meta_value]);
+			$groups[$r->post_id] = array_merge($groups[$r->post_id], $locations[$r->meta_value]);
 		}
 	}
+	$groups[$r->post_id][$r->meta_key] = $r->meta_value;	
+}
+
+//go through the groups and add to the meetings array in the right format
+foreach ($groups as $group_id => $group) {
 	foreach ($days as $day) {
-		if ($r->meta_key == $day . '_new' && !empty($r->meta_value)) {
-			for ($i = 0; $i < $r->meta_value; $i++) {
-				dd()
+		if (!empty($group[$day . '_new'])) {
+			for ($i = 0; $i < $group[$day . '_new']; $i++) {
+
+				//decode types
+				$types = array();
+				if ($group['accessible_venue'] == '1') $types[] = 'X';				
+				if (isset($group[$day . '_new_' . $i . '_meeting_type'])) {
+					$undecoded_types = unserialize($group[$day . '_new_' . $i . '_meeting_type']);
+					foreach ($undecoded_types as $type) {
+						if (array_key_exists($type, $type_lookup)) {
+							if (is_array($type_lookup[$type])) {
+								$types = array_merge($types, $type_lookup[$type]);
+							} else {
+								$types[] = $type_lookup[$type];
+							}
+						}
+					}
+				}
+
+				//append meeting to meetings array
+				$meetings[] = array(
+					'slug' => $group_id . '_' . $day . '_' . $i,
+					'name' => $group['name'],
+					'location' => $group['location'],
+					'updated' => max($group['meeting_updated'], $group['location_updated']),
+					'city' => $group['city'],
+					'state' => $group['state'],
+					'street_address' => $group['street_address'],
+					'notes' => $group['suite_number'],
+					'postal_code' => $group['zip'],
+					'time' => $group[$day . '_new_' . $i . '_meeting_time'],
+					'types' => $types,
+					'url' => 'https://aabroward.org/locations/' . $group['location_slug'] . '/',
+				);
+
 			}
 		}
 	}
-	$meetings[$r->post_id][$r->meta_key] = $r->meta_value;	
 }
 
-dd($meetings);
-
-function getMeta($id, $key) {
-	global $meta;
-	$match = current(array_filter($meta, function($meta) use ($id, $key) {
-		return $meta->post_id == $id && $meta->meta_key == $key;
-	}));
-	return $match ? $match->meta_value : null;
+//encode JSON
+$return = json_encode($meetings);
+if (json_last_error()) {
+	die('JSON error: ' . json_last_error_msg());
 }
 
-//reformat meetings
-$meetings = array_map(function($meeting){
-	global $locations, $meta;
-	
-	$meeting_meta = array_filter($meta, function($meta) use ($meeting) {
-		return $meta->post_id == $meeting->ID;
-	});
+//make sure headers haven't already been sent, will cause error
+if (headers_sent()) {
+	die('Error: headers already sent!');
+}
 
-	$location_id = current(array_filter($meeting_meta, function($meta){
-		return $meta->meta_key == 'meeting_location';
-	}))->meta_value;
-
-	$location = current(array_filter($locations, function($location) use ($location_id) {
-		return $location->ID == $location_id;
-	}));
-
-	if ($access = getMeta($location_id, 'accessible_venue')) {
-		//add H to types
-
-	}
-
-	return array(
-		'slug' => $meeting->ID,
-		'name' => $meeting->post_title,
-		'updated' => $meeting->post_modified_gmt,
-		'location_id' => $location_id,
-		'location' => $location ? $location->post_title : '',
-		'location_notes' => getMeta($location_id, 'suite_number'),
-		'address' => getMeta($location_id, 'street_address'),
-		'city' => getMeta($location_id, 'city'),
-		'state' => getMeta($location_id, 'state'),
-		'postal_code' => getMeta($location_id, 'zip'),
-	);
-}, $meetings);
-
-dd($meetings);
-
+//output
+header('Content-type: application/json; charset=utf-8');
+echo $return;
